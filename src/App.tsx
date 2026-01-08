9583ee6f5db9dec80049ca25824160c144090122
nahidraz23 <nzamanraz@gmail.com>
2026-01-09 00:57:59 +0600

    add: created useSelector and useDispatch hook and displayed dynamic count value on the view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
 function App() {
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelector((state) => state.counter);
+
+  // const handleIncrement = () => {
+  //   dispatch(increment());
+  // };
+
+  // const handleDecrement = () => {
+  //   dispatch(decrement());
+  // };
+
   return (
     <div>
       <h1>Counter with Redux</h1>
-      <button>Increment</button>
-      <div>0</div>
-      <button>Decrement</button>
+
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <div>{count}</div>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
