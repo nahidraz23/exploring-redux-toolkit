3030987ff64741b73f90cbc3b50885a156d295e4
nahidraz23 <nzamanraz@gmail.com>
2026-01-10 20:50:37 +0600

    feat: shadCN installed and button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { decrement, increment } from "./redux/features/counter/counterSlice";
-import { useAppDispatch, useAppSelector } from "./redux/hook";
+// import { decrement, increment } from "./redux/features/counter/counterSlice";
+// import { useAppDispatch, useAppSelector } from "./redux/hook";
+
+import { Button } from "./components/ui/button";
 
 function App() {
-  const dispatch = useAppDispatch();
-  const {count}  = useAppSelector((state) => state.counter);
+  // const dispatch = useAppDispatch();
+  // const {count}  = useAppSelector((state) => state.counter);
 
   // const handleIncrement = () => {
   //   dispatch(increment());
@@ -15,12 +17,13 @@ function App() {
 
   return (
     <div>
-      <h1>Counter with Redux</h1>
+      <h1>Basic ToDo App </h1>
+      <Button>Test</Button>
 
-      <button onClick={() => dispatch(increment(1))}>Increment</button>
+      {/* <button onClick={() => dispatch(increment(1))}>Increment</button>
       <button onClick={() => dispatch(increment(5))}>Increment by 5</button>
       <div>{count}</div>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button> */}
     </div>
   );
 }
