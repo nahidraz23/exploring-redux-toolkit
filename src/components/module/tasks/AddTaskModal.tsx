59527f3ff69f28c48c1a24dcc53c50d21ba25501
nahidraz23 <nzamanraz@gmail.com>
2026-01-11 02:07:51 +0600

    feat: modal added for taking input task data from the user

---
+++ src/components/module/tasks/AddTaskModal.tsx
@@ -0,0 +1,93 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useForm } from "react-hook-form";
+
+const AddTaskModal = () => {
+  const form = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button>Add Task</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-106.25">
+            <DialogDescription className="sr-only">
+                Fill out this form to add task
+            </DialogDescription>
+          <DialogHeader>
+            <DialogTitle>Add Task</DialogTitle>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                        Title
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} value={field.value || " "}/>
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                        Description
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea {...field} value={field.value || " "}/>
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <Button type="submit">Save</Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+};
+
+export default AddTaskModal;
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,5 +1,6 @@
+import AddTaskModal from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
-import { selectFilter, selectTask } from "@/redux/features/task/taskSlice";
+import { selectTask } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
 
 const Tasks = () => {
@@ -10,13 +11,14 @@ const Tasks = () => {
 
   return (
     <div className="flex flex-col gap-10">
-      <div className="mt-10">
+      <div className="flex mt-10 items-center justify-between">
         <h1 className="text-center text-3xl font-bold text-teal-500">Tasks</h1>
+        <AddTaskModal/>
       </div>
 
       <div className="flex flex-col gap-4">
         {tasks.map((task) => (
-          <TaskCard task={task} />
+          <TaskCard task={task} key={task.id}/>
         ))}
       </div>
     </div>
